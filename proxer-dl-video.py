34bc75e6a5ca87c300fd2018cae45b4b3dbef51c
TimHasert <timhasert@gmail.com>
2021-06-08 07:34:07 +0200

    moved utils library to own dir

--- proxer-dl-video.py
+++ proxer-dl-video.py
@@ -1,8 +1,10 @@
-from proxer_utils import download_mp4, get_embed_url, get_mp4_url
+from argparse     import ArgumentParser
+from lib.proxer_utils import download_mp4, get_embed_url, get_mp4_url
+
 
 embed_url = get_embed_url(
     'https://proxer.me/watch/18686/4/engsub',
-    ('e0da4f913f5f05ed7a3f6dc5f0488c7b', '70qn7q3uri1nsqfef9c6jpb03m')
+    ('e0da4f913f5f05ed7a3f6dc5f0488c7b','7ebv928opvp5o6vu82fu81bh5n')
 )
 
 mp4_url = get_mp4_url(embed_url)
