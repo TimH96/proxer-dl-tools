156275d828efb11d1826c821a13623e70e9d7e8e
TimHasert <timhasert@gmail.com>
2021-06-08 15:39:37 +0200

    implemented get_mode for video script

--- proxer-dl-video.py
+++ proxer-dl-video.py
@@ -21,18 +21,25 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         '-o', '--outfile',
-        help='path to output file, include .mp4 extensions',
-        required=True
+        help='path to output file, include .mp4 extensions'
     )
     parser.add_argument(
         '-t', '--token',
         help='auth token cookie, seperated by =, example: --token 4lpHaNuM57r=An0th3r5tr',
         required=True
     )
+    parser.add_argument(
+        '-g', '--get_mode',
+        action='store_true',
+        help="get url mode, collects direct mp4 link and posts it to stdout but doesn't download them",
+    )
     args : Namespace = parser.parse_args()
     # input check
     args.token = check_validity_token(args.token)
     check_validity_file(args.outfile)
+    if not args.get_mode:
+        if not args.outfile:
+            script_exit('--outfile required unless in --get_mode')
     # script execution
     try:
         embed_url = get_embed_url(
@@ -42,12 +49,17 @@ if __name__ == '__main__':
     except TargetNotFoundError:
         script_exit('could not find embed, this is most likely due to an invalid auth token or because there is no proxer upload for this video or DDOS protection')
     mp4_url = get_mp4_url(embed_url)
-    try:
-        download_mp4(
-            mp4_url,
-            args.outfile,
-            args.outfile
-        )
-        print(f'successfully downloaded {args.url}')
-    except KeyboardInterrupt:
-        script_exit('keyboard interrupt, download stopped')
+    # log collected mp4s if in get_mode
+    if args.get_mode:
+        print(mp4_url)
+    # download in normal mode
+    else:
+        try:
+            download_mp4(
+                mp4_url,
+                args.outfile,
+                args.outfile
+            )
+            print(f'successfully downloaded {args.url}')
+        except KeyboardInterrupt:
+            script_exit('keyboard interrupt, download stopped')
